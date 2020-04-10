cfe9d5a00cb19f65e143d5a4a0067576e2a44045
aman-maharshi <amanm4harshi@gmail.com>
2020-04-10 16:06:05 +0530

    minor change

--- project-tracking-intro/script.js
+++ project-tracking-intro/script.js
@@ -1,6 +1,5 @@
 const hamburger = document.getElementById('hamburger');
 const nav = document.getElementById('nav-list');
-const closeIcon = document.getElementById('close-icon');
 
 hamburger.addEventListener('click', (e) => {
     e.preventDefault();
