b6296dfeb59e5fdc77ab54f6205450a1f0db59fd
aman-maharshi <amanm4harshi@gmail.com>
2020-04-20 16:45:01 +0530

    mobile hamburger and cross functionality complete

--- project-tracking-intro/script.js
+++ project-tracking-intro/script.js
@@ -1,7 +1,9 @@
 const hamburger = document.getElementById('hamburger');
 const nav = document.getElementById('nav-list');
+const body = document.body; 
 
 hamburger.addEventListener('click', (e) => {
     e.preventDefault();
-    nav.classList.toggle('show-nav');
+    body.classList.toggle('show-nav');
+    nav.classList.toggle('toggle-flex');
 })
